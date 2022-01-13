80d0b15a28a9462b6719f7e8d3f8d403320482b6
Nadezhda <galkevicnadezda18@gmail.com>
2022-01-13 19:50:03 +0300

    new file

--- js-task_2/kata.js
+++ js-task_2/kata.js
@@ -1,12 +1,37 @@
+ 
 function basicOp(operation, value1, value2)
 {
-  // Code
+  if(operation == '+')return value1 + value2;
+  if(operation == '-')return value1 - value2;
+  if(operation == '*')return value1 * value2;
+  if(operation == '/')return value1 / value2;
 }
+
+let value1 = 4;
+let value2 = 5;
+let sum = basicOp('+', value1, value2);
+let minus = basicOp('-', value1, value2);
+let multiply = basicOp('*', value1, value2);
+let slash = basicOp('/', value1, value2);
+console.log(sum);
+console.log(minus);
+console.log(multiply);
+console.log(slash);
+
+ 
 
 function bonusTime(salary, bonus) {
-   
+  if (bonus == true) return salary10
+  else return salary;
 }
 
-function getGrade (s1, s2, s3) {
+let salary = 5,
+bonus = false;
+let cash = bonusTime(salary, bonus);
+console.log(cash);
+
+ 
+
+function getGrade (s1, s2, s3) 
     
-}
+
