56e8480ca88dd34c55468dfa37cb3978bae31a8d
Nadezhda <galkevicnadezda18@gmail.com>
2022-01-21 21:03:18 +0300

    new

---
+++ -homeFE115js-task_4index.js
@@ -0,0 +1,63 @@
+[33mcommit c10f9180acd54ade3ca39340ea849248a87aae3b[m[33m ([m[1;36mHEAD -> [m[1;32mn.galkevich[m[33m)[m
+Author: Nadezhda <galkevicnadezda18@gmail.com>
+Date:   Fri Jan 14 19:31:02 2022 +0300
+
+    js-task_4
+
+[33mcommit 83de87d424b724e2586f7d01882541054319367e[m
+Merge: 80d0b15 9082871
+Author: Nadezhda <galkevicnadezda18@gmail.com>
+Date:   Thu Jan 13 20:06:57 2022 +0300
+
+    Merge branch 'master' into n.galkevich
+
+[33mcommit 90828718e2d40dbda06ac1f7dd02ed2be1969320[m[33m ([m[1;31morigin/master[m[33m, [m[1;31morigin/HEAD[m[33m, [m[1;32mmaster[m[33m)[m
+Merge: 9c444e3 833dcda
+Author: Halkevich <96432570+Halkevich@users.noreply.github.com>
+Date:   Thu Jan 13 20:02:30 2022 +0300
+
+    Merge pull request #3 from NadezhdaMyITSchool/master
+    
+    js4
+
+[33mcommit 80d0b15a28a9462b6719f7e8d3f8d403320482b6[m[33m ([m[1;31morigin/n.galkevich[m[33m)[m
+Author: Nadezhda <galkevicnadezda18@gmail.com>
+Date:   Thu Jan 13 19:50:03 2022 +0300
+
+    new file
+
+[33mcommit 833dcda1482f0f808afbc957a686c840ad6d873e[m
+Author: Nadya <nadejda.kozhukh@gmail.com>
+Date:   Wed Jan 12 21:42:48 2022 +0300
+
+    js4
+
+[33mcommit a819882360f566c3273f07547bb4b917121bc63e[m
+Merge: 1cd31e2 47d1b55
+Author: Halkevich <96432570+Halkevich@users.noreply.github.com>
+Date:   Tue Jan 11 19:06:13 2022 +0300
+
+    Merge pull request #2 from NadezhdaMyITSchool/master
+    
+    js2-3
+
+[33mcommit 9c444e3c4a4d26d300ad8ea5484d80a308c3418d[m
+Merge: 1cd31e2 47d1b55
+Author: Halkevich <96432570+Halkevich@users.noreply.github.com>
+Date:   Mon Jan 10 21:42:48 2022 +0300
+
+    Merge pull request #1 from NadezhdaMyITSchool/master
+    
+    js2-3
+
+[33mcommit 47d1b55d849e6bb6d90d8adc3285acfc7052abb6[m
+Author: Nadya <nadejda.kozhukh@gmail.com>
+Date:   Sun Jan 9 21:34:16 2022 +0300
+
+    js2-3
+
+[33mcommit 1cd31e28ad24bb94a8f7987aec9ada89df2cb9fe[m[33m ([m[1;31morigin/p.varikash[m[33m, [m[1;31morigin/o.plokhotskaya[m[33m, [m[1;31morigin/m.bokach[m[33m, [m[1;31morigin/l.murashko[m[33m, [m[1;31morigin/e.kulesh[m[33m, [m[1;31morigin/d.yaskov[m[33m, [m[1;31morigin/d.kalganov[m[33m, [m[1;31morigin/a.savchuk[m[33m, [m[1;31morigin/a.plavinskaya[m[33m, [m[1;31morigin/a.novitsky[m[33m, [m[1;31morigin/a.glinskaya[m[33m)[m
+Author: Nadya <nadejda.kozhukh@gmail.com>
+Date:   Mon Dec 20 13:45:45 2021 +0300
+
+    js1
