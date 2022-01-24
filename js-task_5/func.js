dc47b1500fd71745428df8932f61c3bdfb982b27
lidia <7697442@mail.ru>
2022-01-24 19:09:18 +0300

    js5_task 12,13,14,15, bagged 16

---
+++ js-task_5/func.js
@@ -0,0 +1,28 @@
+
+function checkPerson(name,surname,age) {
+    return 'Привет,  ' + name + '     ' + surname + '  с возрастом  ' + age + '  лет';
+}
+
+function calcThree(a,b,c) {
+    return console.log(result = (a - b)/c);
+}
+
+function calcSqrt(k) {
+    return console.log('Квадрат числа:  ' + result1 = Math.pow(k, 2) + '  Куб числа:  ' +  result2 = Math.pow(k, 2)) ;
+}
+
+function max(a,b) {
+    if (a > b) {
+        return console.log(a + ' больше  '+ b)
+    } else {
+        return console.log(b + ' больше  '+ a)
+    }
+}
+
+function min(a,b) {
+    if (a < b) {
+        return console.log(a + ' меньше  '+ b)
+    } else {
+        return console.log(b + ' меньше  '+ a)
+    } 
+}
