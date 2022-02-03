a5a3eeaf622cbd3446e73ddfa9586744d2af76b4
lidia <7697442@mail.ru>
2022-02-03 18:54:04 +0300

    js8_2

--- js-task_8/index.js
+++ js-task_8/index.js
@@ -5,10 +5,27 @@ let Bulb  = function() {
 	this.power = 0;
 	this.lifetime = 0;
     this.rippleFactor = 0;
-    this.pricePerVt = 0;
-    this.Time = 0;
-    this.pricePerMonth = function () {
-        return this.pricePerVt * this.Time * this.power ;
+    // this.pricePerVt = 0;
+    // this.Time = 0;
+    this.getPrice = function() {
+            
+            this.get = function(){
+               this.pricePerVt = +prompt('Введите цену электроэнергии за кВ/ч');
+               this.Time = +prompt('Введите время использования электроэнергии, ч');
+       
+               this.operation();
+       };
+
+       this.operation = function() {
+              this.Price = this.Time * this.pricePerVt * this.power; 
+
+       this.show();
+       };
+        
+       this.show = function () {
+        alert('Вам нужно заплатить: '  + this.Price);
+       };
+       
     };
 }
 
@@ -19,10 +36,6 @@ let svetodiodBulb = new Bulb();
 luminBulb.name = 'Liminisent lamp';
 commonBulb.name = 'Common lamp';
 svetodiodBulb.name = 'Svetodiod lamp';
-
-// console.log(luminBulb);
-// console.log(commonBulb);
-// console.log(svetodiodBulb);
 
 luminBulb.lifetime= 6; //года
 commonBulb.lifetime = 1;
@@ -36,8 +49,13 @@ luminBulb.power= 7; //Вт
 commonBulb.power = 20;
 svetodiodBulb.power= 3;
 
-luminBulb.pricePerVt = 0.2;//BYN
-luminBulb.Time = 60;//часов
+luminBulb.getPrice();
+commonBulb.getPrice();
+svetodiodBulb.getPrice();
+
+
+// luminBulb.pricePerVt = 0.2;//BYN
+// luminBulb.Time = 60;//часов
 
 
 console.log(luminBulb);
@@ -47,41 +65,41 @@ console.log(svetodiodBulb);
 //Объект 2. Калькулятор
 
 
-let Calc = function() {
-    this.get = function(){
-       this.a = +prompt('Введите число a');
-       this.b = +prompt('Введите число b');
-       this.oper = prompt('Введите операцию:+,-,*,/');
+// let Calc = function() {
+//     this.get = function(){
+//        this.a = +prompt('Введите число a');
+//        this.b = +prompt('Введите число b');
+//        this.oper = prompt('Введите операцию:+,-,*,/');
 
-       this.operation();
-    };
+//        this.operation();
+//     };
 
-    this.operation = function() {
-        switch(this.oper) {
-            case '+': 
-                this.result = this.a + this.b;
-            break;
-            case '-': 
-                this.result = this.a - this.b;
-            break;
-            case '*': 
-                this.result = this.a * this.b;
-            break;
-            case '/': 
-                this.result = this.a / this.b;
-            break;
-            default: this.result = 0;
-        }
-        this.show();
-    };
+//     this.operation = function() {
+//         switch(this.oper) {
+//             case '+': 
+//                 this.result = this.a + this.b;
+//             break;
+//             case '-': 
+//                 this.result = this.a - this.b;
+//             break;
+//             case '*': 
+//                 this.result = this.a * this.b;
+//             break;
+//             case '/': 
+//                 this.result = this.a / this.b;
+//             break;
+//             default: this.result = 0;
+//         }
+//         this.show();
+//     };
 
-    this.show = function () {
-        alert(this.a + '  ' + this.oper +  '  ' + this.b   + ' = '  + this.result);
-    };
-};
+//     this.show = function () {
+//         alert(this.a + '  ' + this.oper +  '  ' + this.b   + ' = '  + this.result);
+//     };
+// };
 
-let calc = new Calc();
-calc.get();
+// let calc = new Calc();
+// calc.get();
 
 
 
