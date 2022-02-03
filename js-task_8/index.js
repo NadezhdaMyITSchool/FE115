1ec84497d95b8e3a33b70b86f5a99801c231a8bc
Nadezhda <galkevicnadezda18@gmail.com>
2022-02-03 17:46:20 +0300

    task-8

--- js-task_8/index.js
+++ js-task_8/index.js
@@ -1,176 +1,176 @@
 /*- Калькулятор. Возможные методы: вкл/выкл калькулятора, получение выражения для
 расчета или, а и б числа с операцией, вычисление выражения, показ результата расчета.*/
 
-// let Calc = function () {
-//     this.get = function () {
-//         this.a = +prompt('Введите число a');
-//         this.b = +prompt('Введите число b');
-//         this.oper = prompt('Введите операцию: +, -, *, /');
+let Calc = function () {
+    this.get = function () {
+        this.a = +prompt('Введите число a');
+        this.b = +prompt('Введите число b');
+        this.oper = prompt('Введите операцию: +, -, *, /');
 
-//         this.operation();
-//     };
-//     this.operation = function () {
-//         switch(this.oper){
-//             case '+':
-//                 this.result = this.a + this.b;
-//                 break;
-//                 case '-':
-//                     this.result = this.a - this.b;
-//                     break;
-//                     case '*':
-//                         this.result = this.a * this.b;
-//                         break;
-//                         case '/':
-//                             this.result = this.a / this.b;
-//                             break;
-//                             default: this.result = 0;
-//         }
-//         this.show();
-//     };
-//     this.show = function () {
-//         alert(this.a + ' ' + this.oper + ' ' + this.b + ' = ' + this.result);
-//     }
-// };
+        this.operation();
+    };
+    this.operation = function () {
+        switch(this.oper){
+            case '+':
+                this.result = this.a + this.b;
+                break;
+                case '-':
+                    this.result = this.a - this.b;
+                    break;
+                    case '*':
+                        this.result = this.a * this.b;
+                        break;
+                        case '/':
+                            this.result = this.a / this.b;
+                            break;
+                            default: this.result = 0;
+        }
+        this.show();
+    };
+    this.show = function () {
+        alert(this.a + ' ' + this.oper + ' ' + this.b + ' = ' + this.result);
+    }
+};
 
-// let calc = new Calc();
-// calc.get();
+let calc = new Calc();
+calc.get();
 
 /*2.Лампочка. Возможные методы: ввод информации о лампочке (мощность, стоимость
     электроэнергии), вкл./выкл. лампочки, получение расхода за горение лампочки,
     счетчик горения лампочки.*/
 
-    // let Calc = function (name) {
-    //     this.info = function () {
-    //         this.name = name;
-    //         this.mosh = +prompt('Введите мощность лапочки в Вт'); 
-    //         this.price = 0.209;  //prompt('Введите стоимость эллектроэнергии за 1 Квт', '+', '/', 'ч ');
-    //         if (confirm('Включить лампочку')) {
-    //             this.workTime = +prompt('Сколько времени работает лампочка?')
-    //         }else{
-    //             this.workTime = 0;
-    //         }
-    //         this.operation()
-    //     }
-    //       this.operation = function () {
-    //           this.result =(this.mosh / 1000) * this.price * this.workTime;
-    //           this.show()
+    let Calc = function (name) {
+        this.info = function () {
+            this.name = name;
+            this.mosh = +prompt('Введите мощность лапочки в Вт'); 
+            this.price = 0.209;  //prompt('Введите стоимость эллектроэнергии за 1 Квт', '+', '/', 'ч ');
+            if (confirm('Включить лампочку')) {
+                this.workTime = +prompt('Сколько времени работает лампочка?')
+            }else{
+                this.workTime = 0;
+            }
+            this.operation()
+        }
+          this.operation = function () {
+              this.result =(this.mosh / 1000) * this.price * this.workTime;
+              this.show()
               
-    //       }
-    //       this.show = function () {
+          }
+          this.show = function () {
  
-    //         if (this.workTime == 0 || this.workTime == undefined) {
-    //             console.log('Лампочка "' + this.name + '" не включена');
-    //         } else {
-    //             console.log('Лампочка "' + this.name + '" проработала - ' + this.workTime + 'ч, и стоимость потраченой электроэнергии составляет = ' + this.result + 'р.');
-    //         }
-    //     };
-    // };
+            if (this.workTime == 0 || this.workTime == undefined) {
+                console.log('Лампочка "' + this.name + '" не включена');
+            } else {
+                console.log('Лампочка "' + this.name + '" проработала - ' + this.workTime + 'ч, и стоимость потраченой электроэнергии составляет = ' + this.result + 'р.');
+            }
+        };
+    };
     
 
-//    let calc = new Calc()
-//    calc.info()
+   let calc = new Calc()
+   calc.info()
 /*3. Чайник. Возможные методы: ввод инф. о чайнике (мощность, объем, кол-во воды),
 вкл./выкл., расчет времени закипания воды.*/
-//  let calcWater = function () {
-//      this.get = function (name) {
-//          this.name = name;
-//          this.power = +prompt('Введите мощность чайника');
-//          this.volum =  +prompt('Введите объем чайника');
-//          this.water =  +prompt('Ввести колличество воды ');
-//      if (confirm('Включить чайник')) {
-//          this.workWater = +prompt('Сколько по времени закипает чайник')
-//         }else{
-//             this.workWater = 0;
-//         }
-//         this.operation()
-//      }
-//      this.operation = function () {
-//          this.result = (this.power / 2200) * this.water * this.workWater;
-//            this.show()
-//         } 
-//         this.show = function () {
-//             if (this.workWater == true || this.workWater == false) {
-//                alert('Чайник "' + this.name + '" не включен');
-//             } else {
-//                 alert('Чайник "' + this.name + '" проработал - ' + this.workWater + 'ч, по времени закипает', ' = ' + this.result + 'м');
-//             }
-//             this.show();
-//                 };
-//                 this.show = function () {
-//                    alert(this.power + ' ' + this.operetion + ' ' + this.volum + ' = ' + this.result);
-//                 }
-//   };
+ let calcWater = function () {
+     this.get = function (name) {
+         this.name = name;
+         this.power = +prompt('Введите мощность чайника');
+         this.volum =  +prompt('Введите объем чайника');
+         this.water =  +prompt('Ввести колличество воды ');
+     if (confirm('Включить чайник')) {
+         this.workWater = +prompt('Сколько по времени закипает чайник')
+        }else{
+            this.workWater = 0;
+        }
+        this.operation()
+     }
+     this.operation = function () {
+         this.result = (this.power / 2200) * this.water * this.workWater;
+           this.show()
+        } 
+        this.show = function () {
+            if (this.workWater == true || this.workWater == false) {
+               alert('Чайник "' + this.name + '" не включен');
+            } else {
+                alert('Чайник "' + this.name + '" проработал - ' + this.workWater + 'ч, по времени закипает', ' = ' + this.result + 'м');
+            }
+            this.show();
+                };
+                this.show = function () {
+                   alert(this.power + ' ' + this.operetion + ' ' + this.volum + ' = ' + this.result);
+                }
+  };
 
 
-//  let calkWater = new calcWater()
-//  calkWater.get()
+ let calkWater = new calcWater()
+ calkWater.get()
 
 
  /*Автомобиль. Возможные методы: ввод информации об авто (марка, номер), вкл./выкл.
 двигателя, вкл./выкл. передачи (вперед, назад, нейтральная), движение вперед и назад
 (ввод информации о скорости движения), расчет пройденных километров.*/
 
-// let Car = function() {
-//     this.turn = function() {
-//         this.on = confirm('Вы завели машину?');
-//         if (this.on) {
-//             this.get();
-//         }
-//         else {
-//             alert('Машина не заведена')
-//         }
+let Car = function() {
+    this.turn = function() {
+        this.on = confirm('Вы завели машину?');
+        if (this.on) {
+            this.get();
+        }
+        else {
+            alert('Машина не заведена')
+        }
  
-//     };
+    };
 
-//     this.get = function() {
-//         this.brand = prompt('Введите марку машины');
-//         this.number = prompt('Введите номер машины');
-//         this.transmission = confirm('Вы включили передачу?');
+    this.get = function() {
+        this.brand = prompt('Введите марку машины');
+        this.number = prompt('Введите номер машины');
+        this.transmission = confirm('Вы включили передачу?');
 
-//         if (this.transmission) {
-//             this.onward = confirm('вы ехали вперед?');
-//             if (this.onward) {
-//                 this.onwardV = +prompt('Введите скорость с которой вы ехали вперед в км/ч');
-//                 this.onwardTime = +prompt('Введите время сколько вы ехали вперед в минутах');
-//             }
-//             else {
-//                 alert('Вы наверное хотите поехать в другую сторону');
-//                 this.onwardV = 0;
-//                 this.onwardTime= 0;
-//             } 
+        if (this.transmission) {
+            this.onward = confirm('вы ехали вперед?');
+            if (this.onward) {
+                this.onwardV = +prompt('Введите скорость с которой вы ехали вперед в км/ч');
+                this.onwardTime = +prompt('Введите время сколько вы ехали вперед в минутах');
+            }
+            else {
+                alert('Вы наверное хотите поехать в другую сторону');
+                this.onwardV = 0;
+                this.onwardTime= 0;
+            } 
 
-//             this.down = confirm('может вы ехали еще назад?');
-//             if (this.down) {
-//                 this.downV = +prompt('Введите скорость с которой вы ехали назад в км/ч');
-//                 this.downTime = +prompt('Введите время сколько вы ехали назад в минутах');
-//             }
+            this.down = confirm('может вы ехали еще назад?');
+            if (this.down) {
+                this.downV = +prompt('Введите скорость с которой вы ехали назад в км/ч');
+                this.downTime = +prompt('Введите время сколько вы ехали назад в минутах');
+            }
 
-//             else {
-//                 alert('Правильно,только вперед');
-//                 this.downV = 0;
-//                 this.downTime= 0;
-//             }
-//         }
-//         else {
-//             alert('Включите передачу чтобы поехать')
-//         }
-//         this.distance();
-//     };
+            else {
+                alert('Правильно,только вперед');
+                this.downV = 0;
+                this.downTime= 0;
+            }
+        }
+        else {
+            alert('Включите передачу чтобы поехать')
+        }
+        this.distance();
+    };
 
-//     this.distance = function() {
-//         this.result = this.onwardV+ this.onwardTime + this.downV + this.downTime; 
+    this.distance = function() {
+        this.result = this.onwardV+ this.onwardTime + this.downV + this.downTime; 
 
-//         this.show();
-//     };
+        this.show();
+    };
 
-//     this.show = function() {
-//             alert('Машина ' + this.brand + ' с номером ' + this.number + ' проехала ' + this.result + ' км');
+    this.show = function() {
+            alert('Машина ' + this.brand + ' с номером ' + this.number + ' проехала ' + this.result + ' км');
 
-//     };
-// };
+    };
+};
 
-// let car = new Car();
-// car.turn();
+let car = new Car();
+car.turn();
 
 /*- Контакты. Возм. методы: добавление нового контакта (ввод ФИО, возраст, телефон, эл.
 почта), проверка введенной информации, например: проверить возраст – должен быть
@@ -178,7 +178,7 @@
 контакте, вывод всех контактов.*/
 
 
-let User = function () {
+let user = function () {
     this.name = 'No name'
     this.age = 0;
     this.secName = "No secName"
